d1fa0d1c3fb6776e2adec7430c19231d4de19911
Walber Melo <dev4wm@gmail.com>
2022-09-20 14:54:51 +0100

    feat(CRUD): Implement crud operations

--- MVC/Routes/taskRoutes.js
+++ MVC/Routes/taskRoutes.js
@@ -1,9 +1,13 @@
-const express = require('express')
+const express = require("express");
 const api = express.Router();
 
 const taskController = require("../controllers/taskController");
 
-api.get("/tasks", taskController.getTasks)
-api.post("/tasks", taskController.postTask)
+api.get("/tasks", taskController.getTasks);
+api.get("/tasks/:id", taskController.getTask);
+api.put("/tasks/:id", taskController.putTask);
+api.patch("/tasks/:id", taskController.changeTask);
+api.delete("/tasks/:id", taskController.deleteTask);
+api.post("/tasks", taskController.postTask);
 
 module.exports = api;
--- MVC/controllers/taskController.js
+++ MVC/controllers/taskController.js
@@ -15,8 +15,125 @@ async function postTask(req, res) {
   }
 }
 
-function getTasks(req, res) {
-  res.status(200).send({ msg: "Task Controller it's OK" });
+async function getTasks(req, res) {
+  try {
+    const tasks = await Task.find().sort({ create_at: -1 });
+
+    if (!tasks) {
+      res.status(400).send({ msg: "Error: Cannot get tasks" });
+    } else {
+      res.status(200).send(tasks);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
 }
 
-module.exports = { getTasks, postTask };
+//Get specific task
+async function getTask(req, res) {
+  const taskId = req.params.id;
+
+  try {
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      res.status(400).send({ msg: "Error: Task doesn't exists" });
+    } else {
+      res.status(200).send(task);
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+function putTask(req, res) {
+  const taskId = req.params.id;
+  const params = req.body;
+
+  try {
+    Task.findById(taskId, (err, taskData) => {
+      if (err) {
+        res.status(500).send({ msg: "Server status error" });
+      } else {
+        if (!taskData) {
+          res.status(400).send({ msg: " Error: Task doesn't exists" });
+        } else {
+          // recover params which will be modified
+          taskData.name = params.name;
+          taskData.description = params.description;
+
+          //If there is no error update task
+          Task.findByIdAndUpdate(taskId, taskData, (err, result) => {
+            if (err) {
+              res.status(404).send({ msg: err });
+            } else if (!result) {
+              res.status(404).send({ msg: "Error: task doesn't exists" });
+            } else {
+              res.status(201).send({ task: taskData });
+            }
+          });
+        }
+      }
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+async function deleteTask(req, res) {
+  const taskId = req.params.id;
+
+  try {
+    const task = await Task.findByIdAndDelete(taskId);
+
+    if (!task) {
+      res.status(400).send({ msg: "Error: Task doesn't exists" });
+    } else {
+      res.status(200).send({ msg: "Task successfully deleted" });
+    }
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+function changeTask(req, res) {
+  // recuperar id de la tarea:
+  const taskId = req.params.id;
+
+  try {
+    Task.findById(taskId, (err, taskData) => {
+      if (err) {
+        res.status(500).send({ msg: "Server status error" });
+      } else {
+        if (!taskData) {
+          res.status(400).send({ msg: "Error: Task doesn't exists" });
+        } else {
+          taskData.is_complete = true;
+          taskData.date_finish = Date.now();
+
+          //Change task status to complete
+          Task.findByIdAndUpdate(taskId, taskData, (err, result) => {
+            if (err) {
+              res.status(404).send({ msg: err });
+            } else if (!result) {
+              res.status(404).send({ msg: "Error: user doesn't exists" });
+            } else {
+              res.status(201).send({ task: taskData });
+            }
+          });
+        }
+      }
+    });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+}
+
+module.exports = {
+  getTasks,
+  postTask,
+  getTask,
+  putTask,
+  deleteTask,
+  changeTask,
+};
